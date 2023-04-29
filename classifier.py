11ec4639b9c9c4c5bf27e56b191232dae990acd2
ShaggyBa <semenvanchugov@mail.ru>
2023-04-29 13:45:32 +0300

    +

--- classifier.py
+++ classifier.py
@@ -1,31 +1,39 @@
 import os
-import zipfile
-
+# import zipfile
+import random
 
 os.chdir(os.path.relpath("./Archieve/"))
 
-file_name = 'data.zip'
+# file_name = 'data.zip'
 
-with zipfile.ZipFile(file_name, "r") as archive:
-    archive.extractall(path="./Archieve/")
+# with zipfile.ZipFile(file_name, "r") as archive:
+#     archive.extractall(path="./data/train")
 
 
-def load_training_data(data_directory: str = "data/train", split: float = 0.8, limit: int = 0) -> tuple:
+def load_training_data(data_directory: str = "./data/train", split: float = 0.8, limit: int = 0) -> tuple:
     reviews = []  #
-    # for label in ["pos", "neg"]:  #
-    #     labeled_directory = f"{data_directory}/{label}"  #
-    #     for review in os.listdir(labeled_directory):  #
-    #         if review.endswith(".txt"):  #
-    #             with open(f"{labeled_directory}/{review}") as f:
-    #                 text = f.read()  #
-    #                 text = text.replace("<br />", "\n\n")  #
-    #                 if text.strip():  #
-    #                     spacy_label = {  #
-    #                         "cats": {  #
-    #                             "pos": "pos" == label,  #
-    #                             "neg": "neg" == label  #
-    #                         }  #
-    #                     }  #
-    #                     reviews.append((text, spacy_label))  #
-    #
+    for label in ["pol", "neg"]:  #
+        labeled_directory = f"{data_directory}/{label}"  #
+        for review in os.listdir(labeled_directory):  #
+            if review.endswith(".txt"):  #
+                with open(f"{labeled_directory}/{review}", encoding = "UTF-8") as f:
+                    text = f.read()  #
+                    if text.strip():  #
+                        spacy_label = {  #
+                            "cats": {  #
+                                "pol": "pol" == label,  #
+                                "neg": "neg" == label  #
+                            }  #
+                        }  #
+                        reviews.append((text, spacy_label))  #
 
+    random.shuffle ( reviews )  #
+
+    if limit :  #
+        reviews = reviews [ :limit ]  #
+    split = int ( len ( reviews ) * split )  #
+
+    return reviews [ :split ] , reviews [ split : ]  #
+
+
+print(load_training_data(split = 0.8, limit = 0)[0][0])
--- main.py
+++ main.py
@@ -1,28 +1,16 @@
+import os
+import random
 import spacy
-nlp = spacy.load("en_core_web_sm")
 
-text = """
-Dave watched as the forest burned up on the hill,
-only a few miles from his house. The car had
-been hastily packed and Marta was inside trying to round
-up the last of the pets. "Where could she be?" he wondered
-as he continued to wait for Marta to appear with the pets.
-"""
-
-doc = nlp(text)
-token_list = [token for token in doc]
-
-print(token_list)
-
-filtered_tokens = [token for token in doc if not token.is_stop]
-
-print(filtered_tokens)
-
-lemas = [
-    f"token : {token} | lemma: {token.lemma_}"
-    for token in filtered_tokens
-]
-
-print(lemas)
-
-print(filtered_tokens[1].vector)
+def train_model(
+    training_data: list,
+    test_data: list,
+    iterations: int = 20
+) -> None:
+    # Строим конвейер
+    nlp = spacy.load("en_core_web_sm")
+    if "textcat" not in nlp.pipe_names:
+        textcat = nlp.create_pipe(
+            "textcat", config={"architecture": "simple_cnn"}
+        )
+        nlp.add_pipe(textcat, last=True)
